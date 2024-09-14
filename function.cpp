710245c5ae66036e3a7904cda5d7920a29ceff76
Guilherme <enshihhu.20231@poli.ufrj.br>
2024-09-14 10:52:31 -0300

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #include <stdio.h> //Biblioteca para usar o get char
 #include <ctime> //Biblioteca para medir o tempo de execução
 #include <fstream> //Biblioteca para escrita e leitura de arquivos txt
-#include <sstream> //Biblioteca para manipulação de strings
 #include <stack> //Biblioteca com implementação de pilha
 #include <queue> //Biblioteca com implementação de fila
 #include <vector> //Biblioteca com implementação de array dinâmico
@@ -20,8 +19,9 @@ using namespace std;
 // Variáveis globais
 int V;                          // Número de vértices
 int A = 0;                      // Número de arestas
-vector<vector<int>> adj(INF);   // Lista de adjacências
-vector<vector<int>> mat(V);     // Matriz de adjacências
+vector<vector<int>> adj(V+1);        // Lista de adjacências
+vector<vector<int>> mat(V+1);        // Matriz de adjacências
+bool mode;                      // 0 para lista e 1 para matriz
 
 
 void addEdge(int mode, int v1, int v2) { // mode = 0 para representação em lista e mode = 1 para representação em matriz
@@ -36,7 +36,7 @@ void addEdge(int mode, int v1, int v2) { // mode = 0 para representação em lis
 }
 
 void printListAdj() {
-    for (int i = 0; i < V; ++i) {
+    for (int i = 0; i < V+1; ++i) {
         cout << "Vertice " << i << ":";
         for (int vizinho : adj[i]) {
             cout << " " << vizinho;
@@ -47,8 +47,8 @@ void printListAdj() {
 
 void printMatrizAdj() {
     cout << "Matriz de Adjacencias:" << endl;
-    for (int i = 0; i < V; ++i) {
-        for (int j = 0; j < V; ++j) {
+    for (int i = 0; i < V+1; ++i) {
+        for (int j = 0; j < V+1; ++j) {
             cout << mat[i][j] << " ";
         }
         cout << endl;
@@ -62,30 +62,37 @@ int main() {
     // string FileName; cin >> FileName;
     string FileName = "grafo_1.txt";
     std::ifstream arquivo(FileName);
-    string linha;
 
-    cout << "inicio" << endl;
+    // cin >> mode;
+    mode = 0; // 0 para lista e 1 para matriz
+
+    clock_t start = clock();
+
+    cout << "Pegando dados do txt..." << endl;
     if(!arquivo.is_open()){
         std::cerr << "Não foi possível abrir o arquivo" << "\n";
     }
     else{
         int v1, v2;
         arquivo >> V;
+        if (mode == 0) adj.resize(V+1);
+        else mat.resize(V+1, vector<int>(V+1, 0));
 
-        while(getline(arquivo,linha)){
-            std::istringstream iss(linha);
-            if(iss >> v1 >> v2){
-                addEdge(0, v1, v2);
-                A++;
-            }
+        while(arquivo >> v1 >> v2){
+            addEdge(0, v1, v2);
+            A++;
         }
         arquivo.close();
     }
 
-    cout << "Grafo transferido" << endl;
-    printListAdj();
+    clock_t end = clock();
+
+    cout << "Grafo transferido!" << endl;
+    // printListAdj();
     cout << "Numero de vertices no grafo: " << V << endl;
     cout << "Numero de arestas no grafo: " << A << endl;
+    cout << "Tempo de execucao: " << (double)(end - start) / CLOCKS_PER_SEC << "s" << endl;
+
     return 0;
 
 }
