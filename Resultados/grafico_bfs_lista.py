1f426311220fb50d0e367767b061f8e753eee5c0
Guilherme <enshihhu.20231@poli.ufrj.br>
2024-09-19 13:34:28 -0300

    Update grafico_bfs_lista.py
    escala logaritmica, e dados certos

--- Resultados/grafico_bfs_lista.py
+++ Resultados/grafico_bfs_lista.py
@@ -1,3 +1,43 @@
+# import matplotlib.pyplot as plt
+# """
+# This script generates a bar chart using matplotlib to display the average times of BFS (Breadth-First Search) 
+# for different graphs represented by adjacency lists.
+# Functions:
+#     None
+# Variables:
+#     medias (list of float): A list containing the average times for BFS.
+#     categorias (list of str): A list containing the names of the graphs.
+#     plt (module): The matplotlib.pyplot module used for plotting.
+# Usage:
+#     Run the script to display a bar chart with the average BFS times for each graph.
+# Color Options:
+#     RGB tuples: (R, G, B) where R, G, B are floats between 0 and 1.
+# """
+
+# # Valores das médias
+# medias = [0.000360107, 0.00164097, 0.00757983, 0.0394116, 0.176305, 0.38547]
+
+# # Valores do eixo x
+# categorias = ['grafo_1', 'grafo_2', 'grafo_3', 'grafo_4', 'grafo_5', 'grafo_6']
+
+# # Criar o gráfico de barras
+# plt.figure(figsize=(10, 5))
+# plt.bar(categorias, medias, color=(139/255, 169/255, 1))  # Alterar a cor para verde
+
+# # Adicionar título e rótulos aos eixos
+# plt.title('Gráfico de Médias de Tempo da BFS por Lista de Adjacência')
+# plt.xlabel('Grafos')
+# plt.ylabel('Médias (segundos)')
+
+# # Ajustar a escala do eixo y
+# plt.ylim(0, 0.5)  # Ajuste os valores conforme necessário
+
+# # Exibir o gráfico
+# plt.grid(True)
+# plt.show()
+
+
+
 import matplotlib.pyplot as plt
 """
 This script generates a bar chart using matplotlib to display the average times of BFS (Breadth-First Search) 
@@ -15,7 +55,7 @@ Color Options:
 """
 
 # Valores das médias
-medias = [0.0512733, 0.0682073, 0.50033, 0.53074, 3.64389, 4.19819]
+medias = [0.000360107, 0.00164097, 0.00757983, 0.0394116, 0.176305, 0.38547]
 
 # Valores do eixo x
 categorias = ['grafo_1', 'grafo_2', 'grafo_3', 'grafo_4', 'grafo_5', 'grafo_6']
@@ -29,9 +69,9 @@ plt.title('Gráfico de Médias de Tempo da BFS por Lista de Adjacência')
 plt.xlabel('Grafos')
 plt.ylabel('Médias (segundos)')
 
-# Ajustar a escala do eixo y
-plt.ylim(0, 5)  # Ajuste os valores conforme necessário
+# Ajustar a escala do eixo y para logarítmica
+plt.yscale('log')
 
 # Exibir o gráfico
-plt.grid(True)
+plt.grid(True, which="both", ls="--")
 plt.show()
