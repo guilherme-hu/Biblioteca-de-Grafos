752592ad272bf1b77a6396d6ed2fa211abcec9e5
Guilherme <enshihhu.20231@poli.ufrj.br>
2024-09-20 20:49:50 -0300

    Update main.cpp
    atualização do template inicial com os prints no terminal das informações que de podem obter usar pela biblioteca -> tutorial de uso da biblioteca

--- main.cpp
+++ main.cpp
@@ -6,135 +6,44 @@
 #include <vector>
 #include <fstream>
 
-// void testDFS(const std::string& filename, std::vector<double>& times) {
-//     std::ifstream file(filename);
-//     int first_vertex;
-//     file >> first_vertex;
-//     file.close();
 
-//     std::random_device rd;
-//     std::mt19937 gen(rd());
-//     std::uniform_int_distribution<> dis(1, first_vertex);
-
-//     Grafo g(filename, 0);
-
-//     for (int i = 0; i < 100; ++i) {
-//         int random_vertex = dis(gen);
-
-//         auto start = std::chrono::high_resolution_clock::now();
-//         g.dfs(random_vertex);
-//         auto end = std::chrono::high_resolution_clock::now();
-
-//         std::chrono::duration<double> duration = end - start;
-//         times.push_back(duration.count());
-//     }
-//     std::cout << "testDFS for " << filename << " passed 100 times!" << std::endl;
-//     cout << endl;
-// }
-
-// int main() {
-
-//     cin.tie(NULL);
-//     ios_base::sync_with_stdio(false);
-
-//     std::vector<double> times_grafo_1, times_grafo_2, times_grafo_3, times_grafo_4, times_grafo_5, times_grafo_6;
-
-//     testDFS("grafo_1.txt", times_grafo_1); 
-//     testDFS("grafo_2.txt", times_grafo_2); 
-//     testDFS("grafo_3.txt", times_grafo_3); 
-//     testDFS("grafo_4.txt", times_grafo_4); 
-//     testDFS("grafo_5.txt", times_grafo_5); 
-//     testDFS("grafo_6.txt", times_grafo_6); 
-
-//     std::cout << "All DFS tests passed 100 times!" << std::endl;
-//     cout << endl;
-
-//     // Function to calculate mean
-//     auto calculateMean = [](const std::vector<double>& times) {
-//         double sum = 0.0;
-//         for (const auto& time : times) {
-//             sum += time;
-//         }
-//         return sum / times.size();
-//     };
-
-//     // Function to calculate standard deviation
-//     auto calculateStdDev = [](const std::vector<double>& times, double mean) {
-//         double sum = 0.0;
-//         for (const auto& time : times) {
-//             sum += (time - mean) * (time - mean);
-//         }
-//         return std::sqrt(sum / times.size());
-//     };
-
-//     // Save times and statistics to a file
-//     std::ofstream outFile("times_and_stats_DFS.txt");
-
-//     auto saveTimesAndStats = [&](const std::string& name, const std::vector<double>& times) {
-//         double mean = calculateMean(times);
-//         double stdDev = calculateStdDev(times, mean);
-        
-//         outFile << "Times for " << name << ":\n";
-//         for (const auto& time : times) {
-//             outFile << time << " seconds\n";
-//         }
-//         outFile << "Mean: " << mean << " seconds\n";
-//         outFile << "Standard Deviation: " << stdDev << " seconds\n\n";
-//     };
-
-//     saveTimesAndStats("grafo_1", times_grafo_1);
-//     saveTimesAndStats("grafo_2", times_grafo_2);
-//     saveTimesAndStats("grafo_3", times_grafo_3);
-//     saveTimesAndStats("grafo_4", times_grafo_4);
-//     saveTimesAndStats("grafo_5", times_grafo_5);
-//     saveTimesAndStats("grafo_6", times_grafo_6);
-
-//     outFile.close();
-
-//     return 0;
-// }
-
-
-// // main para testes gerais
+// main para testes gerais
 int main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
     
-    string FileName = "grafo_teste.txt";
-    int mode = 1; 
-
     clock_t start = clock();
 
-    Grafo g(FileName, mode);
+    std::string FileName = "grafo_teste.txt";   // Nome do arquivo txt
+    int mode = 0;                               // 0 para lista de adjacência e 1 para matriz de adjacência
+
+    Grafo g(FileName, mode);                    // Criação do objeto grafo
     // if(mode == 0) g.printListAdj(); 
     // else g.printMatrizAdj();
 
-    cout << "Vertices: " << g.getV() << endl; 
-    cout << "Aresta: " << g.getA() << endl;
-    cout << "Grau maximo: " << g.getGrauMax() << endl; 
-    cout << "Grau minimo: " << g.getGrauMin() << endl;    
-    cout << "Grau mediano: " << g.getGrauMediano() << endl;
-    cout << "Grau medio: " << g.getGrauMedio() << endl;
+    cout << "Vertices: " << g.getV() << endl;                   // Print do número de vértices
+    cout << "Aresta: " << g.getA() << endl;                     // Print do número de arestas
+    cout << "Grau maximo: " << g.getGrauMax() << endl;          // Print do grau máximo
+    cout << "Grau minimo: " << g.getGrauMin() << endl;          // Print do grau mínimo
+    cout << "Grau mediano: " << g.getGrauMediano() << endl;     // Print do grau mediano
+    cout << "Grau medio: " << g.getGrauMedio() << endl;         // Print do grau médio
 
-    for (int i = 0; i < g.getCompCon().size(); i++){
-        cout << "Componente conexa " << i+1 << ": " << g.getCompCon()[i].first << " vertices" << endl;
-    }
+    g.printCompCon();                                        // Print do número de componentes conexas e o tamanho de cada uma
     
-    g.bfs(17);
-    g.dfs(17);
-    cout << "Distancia: " << g.distancia(7, 14) << endl;
+    g.bfs(10);                                              // BFS a partir do vértice 10
+    g.dfs(10);                                              // DFS a partir do vértice 10
+    cout << "Distancia: " << g.distancia(10, 15) << endl;   // Distância entre os vértices 10 e 15
 
-    cout << "Diametro: " << g.diametro() << endl;
+    cout << "Diametro: " << g.diametro_aprox() << endl;     // Diâmetro do grafo de forma aproximada
 
     clock_t end = clock();
 
-    cout << "Tempo de execucao: " << (double)(end - start) / CLOCKS_PER_SEC << "s" << endl;
+    cout << "Tempo de execucao: " << (double)(end - start) / CLOCKS_PER_SEC << "s" << endl; // Tempo de execução
 
     return 0;   
 
 }
 
 
-
-//  g++ bib_grafo.cpp main.cpp -o main -O2
+//  g++ main.cpp -o main -O3
 // ./main
